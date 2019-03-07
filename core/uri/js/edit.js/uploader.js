3cc519045e7e0dbeb1677ae19f16b168ec253ffc
Roger <roger@volst.nl>
2019-03-07 18:24:03 +0100

    Change upload text

--- core/uri/js/edit.js/uploader.js
+++ core/uri/js/edit.js/uploader.js
@@ -3,141 +3,181 @@ var uploader = {};
 uploader.count = 0;
 
 uploader.init_uploader = function(elem, uid) {
-    if (elem.data('uid') === uid) {
-        return;
-    }
-    var settings = elem.data('upload');
-    console.log('uploader.init_uploader', elem, uid, settings);
-    opts = {
-        'uid': uid,
-        'field': settings.field,
-        'elem': elem,
-        'file_reader': new FileReader(),
-        'preview': !!settings.preview,
-        'trigger': settings.trigger
-    }
-    opts.file_reader.onload = function(e) {
-        uploader.handle_load(e, opts);
-        elem.prop('disabled', true);
-    }
-    elem.attr('id', uid);
-    elem.data('uid', uid);
-    elem.after('<input type="file" name="file" id="' + uid + '-file" class="nb-close">');
-    if (opts.field) {
-        elem.after('<div id="' + uid + '-field" class="nb-close" data-edit-field="' + opts.field + '"></div>')
-    }
-    $('#' + uid + '-file').change(function(e) {
-        uploader.handle_change(e, opts);
-    });
-}
+  if (elem.data('uid') === uid) {
+    return;
+  }
+  var settings = elem.data('upload');
+  console.log('uploader.init_uploader', elem, uid, settings);
+  opts = {
+    uid: uid,
+    field: settings.field,
+    elem: elem,
+    file_reader: new FileReader(),
+    preview: !!settings.preview,
+    trigger: settings.trigger,
+  };
+  opts.file_reader.onload = function(e) {
+    uploader.handle_load(e, opts);
+    elem.prop('disabled', true);
+  };
+  elem.attr('id', uid);
+  elem.data('uid', uid);
+  elem.after(
+    '<input type="file" name="file" id="' + uid + '-file" class="nb-close">'
+  );
+  if (opts.field) {
+    elem.after(
+      '<div id="' +
+        uid +
+        '-field" class="nb-close" data-edit-field="' +
+        opts.field +
+        '"></div>'
+    );
+  }
+  $('#' + uid + '-file').change(function(e) {
+    uploader.handle_change(e, opts);
+  });
+};
 
 uploader.remove_preview = function(opts) {
-    $('#' + opts.uid + '-preview').remove();
-}
+  $('#' + opts.uid + '-preview').remove();
+};
 
 uploader.add_preview = function(opts) {
-    uploader.remove_preview(opts);
-    if (!opts.preview) {
-        return;
-    }
-    var uid = opts.uid;
-    opts.elem.after('<div id="' + uid + '-preview" class="uploader img-wrapper"></div>');
-    $('#' + opts.uid + '-preview')
-        .append($('<img>',{id: opts.uid + '-img', src:opts.img_preview }))
-        .append('<div class="progress-bar-bg nb-close" id="' + uid + '-bg"><div class="progress-bar" id="' + uid + '-bar"></div></div>')
-        .append('<div class="progress-bar-text nb-close" id="' + uid + '-msg">Uploading</div>')
-        .append('<a id="' + uid + '-clear" data-uid="' + uid + '" href="javascript:void(0)" data-onclick="uploader_clear_img" class="clear-img-icon nb-button icon-button delete"></a>');
-}
+  uploader.remove_preview(opts);
+  if (!opts.preview) {
+    return;
+  }
+  var uid = opts.uid;
+  opts.elem.after(
+    '<div id="' + uid + '-preview" class="uploader img-wrapper"></div>'
+  );
+  $('#' + opts.uid + '-preview')
+    .append($('<img>', { id: opts.uid + '-img', src: opts.img_preview }))
+    .append(
+      '<div class="progress-bar-bg nb-close" id="' +
+        uid +
+        '-bg"><div class="progress-bar" id="' +
+        uid +
+        '-bar"></div></div>'
+    )
+    .append(
+      '<div class="progress-bar-text nb-close" id="' +
+        uid +
+        '-msg">Uploading</div>'
+    )
+    .append(
+      '<a id="' +
+        uid +
+        '-clear" data-uid="' +
+        uid +
+        '" href="javascript:void(0)" data-onclick="uploader_clear_img" class="clear-img-icon nb-button icon-button delete"></a>'
+    );
+};
 
 function uploader_clear_img(e) {
-    uploader.clear_img(e);
+  uploader.clear_img(e);
 }
 
 uploader.clear_img = function(e) {
-    var uid = $(e.target).data('uid');
-    uploader.remove_preview({"uid": uid});
-    $('#' + uid).prop('disabled', false);
-}
+  var uid = $(e.target).data('uid');
+  uploader.remove_preview({ uid: uid });
+  $('#' + uid).prop('disabled', false);
+};
 
 uploader.handle_change = function(e, opts) {
-    console.log('uploader.handle_change', e, opts);
-    var files = e.target.files || e.dataTransfer.files;
-    if (!files || files.length !== 1) {
-        return;
-    }
-    var file = files[0];
-    opts.file_reader.readAsDataURL(file);
-    uploader.set_progress(opts.uid, 0, 'Uploading');
-    uploader.upload_file(opts, file);
-}
+  console.log('uploader.handle_change', e, opts);
+  var files = e.target.files || e.dataTransfer.files;
+  if (!files || files.length !== 1) {
+    return;
+  }
+  var file = files[0];
+  opts.file_reader.readAsDataURL(file);
+  uploader.set_progress(opts.uid, 0, 'Uploading');
+  uploader.upload_file(opts, file);
+};
 
 uploader.handle_load = function(e, opts) {
-    console.log('handle_load', e, opts);
-    opts.img_preview = e.target.result;
-    elem = $(opts.elem);
-    if (elem.is('img')) {
-        elem.attr('src', e.target.result);
-    } else if (opts.preview) {
-        uploader.add_preview(opts);
-    }
-}
+  console.log('handle_load', e, opts);
+  opts.img_preview = e.target.result;
+  elem = $(opts.elem);
+  if (elem.is('img')) {
+    elem.attr('src', e.target.result);
+  } else if (opts.preview) {
+    uploader.add_preview(opts);
+  }
+};
 
 uploader.set_progress = function(uid, progress, msg) {
-    $('#' + uid + '-bg').removeClass('nb-close');
-    $('#' + uid + '-bar').removeClass('nb-close').css('width', progress + '%');
-    $('#' + uid + '-msg').removeClass('nb-close').text(msg);
-}
+  $('#' + uid + '-bg').removeClass('nb-close');
+  $('#' + uid + '-bar')
+    .removeClass('nb-close')
+    .css('width', progress + '%');
+  $('#' + uid + '-msg')
+    .removeClass('nb-close')
+    .text(msg);
+};
 
 uploader.unique_id = function($uploader) {
-    if ($uploader.data('uid')) {
-        return $uploader.data('uid');
-    }
-    uploader.count++;
-    return "uploader-" + uploader.count;
-}
+  if ($uploader.data('uid')) {
+    return $uploader.data('uid');
+  }
+  uploader.count++;
+  return 'uploader-' + uploader.count;
+};
 
 uploader.upload_file = function(opts, file) {
-    var formData = new FormData();
-    formData.append('file', file);
-    $.ajax({
-        url: base_url + '/api/v1/.files',
-        type: 'POST',
-        data: formData,
-        cache: false,
-        contentType: false,
-        processData: false,
-        xhr: function() {
-            var myXhr = $.ajaxSettings.xhr();
-            if (myXhr.upload) {
-                myXhr.upload.addEventListener('progress', function(e) {
-                    if (e.lengthComputable) {
-                        uploader.set_progress(opts.uid, parseInt(100 * e.loaded / e.total), 'Uploading');
-                    }
-                }, false);
+  var formData = new FormData();
+  formData.append('file', file);
+  $.ajax({
+    url: base_url + '/api/v1/.files',
+    type: 'POST',
+    data: formData,
+    cache: false,
+    contentType: false,
+    processData: false,
+    xhr: function() {
+      var myXhr = $.ajaxSettings.xhr();
+      if (myXhr.upload) {
+        myXhr.upload.addEventListener(
+          'progress',
+          function(e) {
+            if (e.lengthComputable) {
+              uploader.set_progress(
+                opts.uid,
+                parseInt((100 * e.loaded) / e.total),
+                'Uploading'
+              );
             }
-            return myXhr;
-        }
-    }).done(function(json) {
-            uploader.set_progress(opts.uid, 100, 'Uploaded');
-            if (opts.field) {
-                $('#' + opts.uid + '-field').text(json.files.uuid);
-            }
-            $('#preview_' + opts.uid).attr('data-uuid', json.files.uuid);
-            if (opts.trigger) {
-                $('#preview_' + opts.uid).trigger('autoselect', json.files);
-            }
-    }).fail(function() {
-        uploader.set_progress(opts.uid, 0, 'Failed');
+          },
+          false
+        );
+      }
+      return myXhr;
+    },
+  })
+    .done(function(json) {
+      uploader.set_progress(opts.uid, 100, 'Done');
+      if (opts.field) {
+        $('#' + opts.uid + '-field').text(json.files.uuid);
+      }
+      $('#preview_' + opts.uid).attr('data-uuid', json.files.uuid);
+      if (opts.trigger) {
+        $('#preview_' + opts.uid).trigger('autoselect', json.files);
+      }
+    })
+    .fail(function() {
+      uploader.set_progress(opts.uid, 0, 'Failed');
     });
-}
+};
 
 $('body').on('click', '[data-upload]', function(e) {
-    e.preventDefault();
-    $this = $(this);
-    if ($this.prop('disabled')) {
-        return;
-    }
-    var uid = uploader.unique_id($this);
-    uploader.init_uploader($this, uid);
-    $('#' + uid + '-file').click();
+  e.preventDefault();
+  $this = $(this);
+  if ($this.prop('disabled')) {
+    return;
+  }
+  var uid = uploader.unique_id($this);
+  uploader.init_uploader($this, uid);
+  $('#' + uid + '-file').click();
 });
