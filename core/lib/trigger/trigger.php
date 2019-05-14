cfba39b98ed437ad915b531e1dcfc360e85e11c9
Hermen Reitsma <hajewiet@gmail.com>
2019-05-14 13:03:17 -0300

    Update reset password

--- core/lib/trigger/trigger.php
+++ core/lib/trigger/trigger.php
@@ -6,37 +6,37 @@ function trigger_sc($params) {
 
 // trigger in all contrib and ext modules having a (modulename)-on-(eventname) lib function
 function trigger($event, $data) {
-	global $SYSTEM;
-	$envs = $SYSTEM['env_paths'];
-	foreach ($envs as $env_path) {
-		if ($env_path === 'core') {
-			continue;
-		}
-		$base_path = $SYSTEM['file_base'] . $env_path . '/modules/';
-		if (!file_exists($base_path)) {
-			continuie;
-		}
+        global $SYSTEM;
+        $envs = $SYSTEM['env_paths'];
+        foreach ($envs as $env_path) {
+        	if ($env_path === 'core') {
+        		continue;
+        	}
+        	$base_path = $SYSTEM['file_base'] . $env_path . '/modules/';
+        	if (!file_exists($base_path)) {
+        		continuie;
+        	}
 
-		$modules = scandir($base_path);
-        foreach ($modules as $module) {
-        	if ($module[0] === '.') {
-        		continue;
-        	}
-        	$hname = $module . '-on-' . $event;
-        	$lib_path = $base_path . $module . '/lib/' . $hname;
-        	if (!file_exists($lib_path)) {
-        		continue;
-        	}
-        	$file = $lib_path . '/' . $hname . '.php';
-        	if (!file_exists($file)) {
-        		continue;
-        	}
-        	require_once($file);
-        	$function_name = str_replace("-", "_", $hname);
-        	if (!function_exists($function_name)) {
-        		continue;
-        	}
-        	$function_name($data);
-		}
-	}
+        	$modules = scandir($base_path);
+                foreach ($modules as $module) {
+                	if ($module[0] === '.') {
+                		continue;
+                	}
+                	$hname = $module . '-on-' . $event;
+                	$lib_path = $base_path . $module . '/lib/' . $hname;
+                	if (!file_exists($lib_path)) {
+                		continue;
+                	}
+                	$file = $lib_path . '/' . $hname . '.php';
+                	if (!file_exists($file)) {
+                		continue;
+                	}
+                	require_once($file);
+                	$function_name = str_replace("-", "_", $hname);
+                	if (!function_exists($function_name)) {
+                		continue;
+                	}
+                	$function_name($data);
+                }
+        }
 }
--- core/modules/user/tpl/email-password-reset/index.tpl
+++ core/modules/user/tpl/email-password-reset/index.tpl
@@ -2,7 +2,7 @@
 <h1>Reset your password</h1>
 <p>Hi [get name]!</p>
 <p>It looks like you’ve forgotten your password at [site-name]. Don’t worry, you can reset your password by clicking the link below.</p>
-<p><a href="[url]">[url]</a></p>
+<p><a href="[reset-url]">[reset-url]</a></p>
 <p>If it was not you who requested to reset your password, just ignore this email. You do not need to do anything and can login to [site-name] as usual.</p>
 <p>Thank you,<br>
 The [site-name] team.</p>
