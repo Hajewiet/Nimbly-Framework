a186e1d1ca9f0cddd7de2a60d557f7636beb75c8
Hermen Reitsma <hajewiet@gmail.com>
2019-05-15 09:38:10 -0300

    Improve logout

--- core/modules/user/lib/logout/logout.php
+++ core/modules/user/lib/logout/logout.php
@@ -2,12 +2,9 @@
 
 function logout_sc($params) {
     run_library('session');
-    run_library('redirect');
+    load_libraries(['redirect', 'text', 'system-messages']);
     $_SESSION = array();
-    $_SESSION['roles'] = array('anonymous' => true);
-    $_SESSION['features'] = array();
-    $_SESSION['username'] = 'anonymous';
-    $_SESSION['SYSTEM']['messages'][] = '[text user_logged_out]';
-    $redirect_url = get_param_value($params, "redirect", "");
-    redirect($redirect_url);
+    session_initialize();
+    system_message(text_sc(['text' => 'user_logged_out']));
+    redirect(get_param_value($params, "redirect", ""));
 }
