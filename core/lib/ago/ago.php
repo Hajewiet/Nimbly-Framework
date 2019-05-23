095877face204caf7a8ab29d72120ae380dfc501
Hermen Reitsma <hajewiet@gmail.com>
2019-05-23 10:40:06 -0300

    Update ago lib

--- core/lib/ago/ago.php
+++ core/lib/ago/ago.php
@@ -12,19 +12,28 @@ function ago_sc($params) {
     $now = time();
     $elapsed = $now - $dt;
     $days = $elapsed / (24*60*60);
+    $years = $days / 365;
+    if ($years > 1) {
+        return $years < 2 ? '1 year ago' : round($years) . ' years ago';
+    }
+    $months = $days / 30;
+    if ($months > 1) {
+        return $months < 2? '1 month ago' : round($months) . ' months ago';
+    }
+    $weeks = $days / 7;
+    if ($weeks > 1) {
+        return $weeks < 2? '1 week ago' : round($weeks) . ' weeks ago';
+    }
     if ($days > 1) {
-        return round($days) . " days ago";
+        return $days < 2? '1 day ago' : round($days) . ' days ago';
     }
     $hours = $elapsed / (60*60);
     if ($hours > 1) {
-        return round($hours) . " hours ago";
+        return $hours < 2? '1 hour ago' : round($hours) . ' hours ago';
     }
     $minutes = $elapsed / 60;
     if ($minutes > 1) {
-        return round($minutes) . " minutes ago";
+        return $minutes < 2? '1 minute ago' : round($minutes) . ' minutes ago';
     }
-    if ($elapsed < 1) {
-        return "1 second ago";
-    }
-    return round($elapsed) . " seconds ago";        
+    return $elapsed < 2? '1 second ago' : round($elapsed) . ' seconds ago';       
 }
